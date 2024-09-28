a2b07f9dc9ff1e3fca98920a6fd8d9f00e19f62e
Emerson <emersonlvs10@gmail.com>
2024-09-28 17:07:35 -0300

    Refactorizar estilos de componente Task e atualizar criação de tarefa

--- my-vers/MyProject/src/components/Task/styles.ts
+++ my-vers/MyProject/src/components/Task/styles.ts
@@ -1,38 +1,38 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
-export const Container =styled.TouchableOpacity`
-    width: 100%;
-    height: 66px;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #161616;
-    overflow: hidden;
-    border-radius: 4px;
-    margin-top: 20px;
+export const Container = styled.TouchableOpacity`
+  width: 100%;
+  height: 60px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #161616;
+  overflow: hidden;
+  border-radius: 4px;
+  margin-top: 20px;
 `;
 
-export const TaskText=styled.Text`
-color: white;
-font-size: 16px;
-font-weight: 500;
+export const TaskText = styled.Text`
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
-export const TaskDone=styled.TouchableOpacity`
-width: 51px;
-height: 31px;
-background-color: #343333;
-justify-content: center;
-align-items: start;
-border-radius: 100px;
-`
+export const TaskDone = styled.TouchableOpacity`
+  margin-left: 8px;
+  width: 51px;
+  height: 31px;
+  background-color: #343333;
+  justify-content: center;
+  align-items: start;
+  border-radius: 100px;
+`;
 
-export const TaskDelete=styled.TouchableOpacity`
-width: 56px;
-height: 62px;
-background-color: #c91025;
-justify-content: center;
-align-items: center;
-border-radius: 4px;
-`
-
+export const TaskDelete = styled.TouchableOpacity`
+  width: 56px;
+  height: 62px;
+  background-color: #c91025;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+`;
