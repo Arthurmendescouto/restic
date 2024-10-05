e4886c9f87a092f666840dffffee58cd9fad2239
ArthurMendes <153393682+Arthurmendescouto@users.noreply.github.com>
2024-10-05 16:25:21 -0300

    menu

--- my-vers/MyProject/App.tsx
+++ my-vers/MyProject/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import Menu from "./src/screens/Menu"; 
 import Home from "./src/screens/Home";
 import Details from "./src/screens/Details";
 import { TaskProvider } from "./src/context/TaskContext";
@@ -13,11 +13,11 @@ export default function App() {
     <TaskProvider>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Home"
+          initialRouteName="Menu"
           screenOptions={{ headerShown: false }}
         >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Details" component={Details} />
+          <Stack.Screen name="Home" component={Menu} />
+          <Stack.Screen name="Details" component={Menu} />
         </Stack.Navigator>
       </NavigationContainer>
     </TaskProvider>
